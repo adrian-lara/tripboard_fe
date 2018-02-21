b9f3abdf9384332f504ac961a188b4d115e5a649
Adrian Lara <adrianblara@gmail.com>
2018-02-21 01:19:05 -0700

    Add styled Title component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../stylesheets/App.css';
 
+import Title from './Title'
 import Welcome from './Welcome'
 import SearchBar from './SearchBar'
 
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Title/>
         <Welcome/>
         <SearchBar/>
       </div>
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../stylesheets/Title.css'
+
+const Title = () => {
+  return (
+    <p className="main-title">
+      Tripboard
+    </p>
+  )
+}
+
+export default Title
