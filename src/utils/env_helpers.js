529180b4b9a637e811296672823ec59009f38521
Adrian Lara <adrianblara@gmail.com>
2018-02-24 19:36:26 -0700

    remove console log from env_helpers

--- src/utils/env_helpers.js
+++ src/utils/env_helpers.js
@@ -1,5 +1,4 @@
 function beRoot() {
-  console.log(process.env)
   if (process.env.REACT_APP_IN_CLUSTER) {
     return "http://192.168.99.100"
   } else if (process.env.NODE_ENV === 'development') {
