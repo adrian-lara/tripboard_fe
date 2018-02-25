12296a0517f117f113df2f1069da6e44be4a3197
Adrian Lara <adrianblara@gmail.com>
2018-02-24 19:40:55 -0700

    App receives getInfo method from SearchBar and gets place info and renders PlaceInfo

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,43 @@ import '../stylesheets/App.css';
 import Title from './Title'
 import Welcome from './Welcome'
 import SearchBar from './SearchBar'
+import PlaceInfo from './PlaceInfo'
+
+import { beRoot, bePort } from '../utils/env_helpers'
+import handleResponse from '../utils/handle-response'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      placeInfo: {},
+    }
+  }
+
+  getInfo = (place_id) => {
+    fetch(`${beRoot()}:${bePort()}/api/v1/basic-info?place_id=${place_id}`)
+      .then(response => handleResponse(response))
+      .then(placeInfo => this.setState({placeInfo: placeInfo}))
+      .catch(error => console.error({ error }))
+  }
+
+  displayInfo = () => {
+    if (Object.keys(this.state.placeInfo).length > 0) {
+      return <PlaceInfo
+              basicInfo={this.state.placeInfo}/>
+    } else {
+      return null
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <Title/>
         <Welcome/>
-        <SearchBar/>
+        <SearchBar
+         getInfo={this.getInfo}/>
+        { this.displayInfo() }
       </div>
     );
   }
