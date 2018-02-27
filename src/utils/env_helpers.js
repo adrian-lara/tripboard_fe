61dcd8d85a5a1675498af3f4a2efdd5ccdc74f61
Adrian Lara <adrianblara@gmail.com>
2018-02-27 00:53:59 -0700

    Reference production BE root:port endpoint via env variable

--- src/utils/env_helpers.js
+++ src/utils/env_helpers.js
@@ -1,5 +1,7 @@
 function beRoot() {
-  if (process.env.REACT_APP_IN_CLUSTER) {
+  if (process.env.REACT_APP_IN_PRODUCTION) {
+    return "http://104.197.74.32"
+  } else if (process.env.REACT_APP_IN_CLUSTER) {
     return "http://192.168.99.100"
   } else if (process.env.NODE_ENV === 'development') {
     return "http://localhost"
@@ -7,7 +9,9 @@ function beRoot() {
 }
 
 function bePort() {
-  if (process.env.REACT_APP_IN_CLUSTER) {
+  if (process.env.REACT_APP_IN_PRODUCTION) {
+    return "8080"
+  } else if (process.env.REACT_APP_IN_CLUSTER) {
     return "30003"
   } else if (process.env.NODE_ENV === 'development') {
     return "8080"
